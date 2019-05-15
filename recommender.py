5f1725d6a0d940f4ca5c0632d97194b3cf2353a5
Vedran Marinović <46447437+vedranmarinovic@users.noreply.github.com>
2019-05-15 15:23:52 +0200

    methodical switch to content based

--- recommender.py
+++ recommender.py
@@ -1,5 +1,5 @@
 from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
-
+import sklearn.metrics.pairwise as pw
 import random
 import pandas as pd
 import numpy as np
@@ -32,49 +32,9 @@ def eat_place(user_id=None, n=10):
     
     return l
 
-def utilityMatrix(USERS, BUSINESSES, REVIEWS, CITIES):
-    business_id = [business["business_id"] for city in CITIES for business in BUSINESSES[city]]
-    user_id = [user["user_id"] for city in CITIES for user in USERS[city]]
+def pivot_ratings(df, REVIEWS):
+    return df.pivot(values=REVIEWS["stars"], columns= REVIEWS['user_id'], index=REVIEWS["business_id"])
 
-    matrix= pd.DataFrame(np.nan, columns= user_id, index= business_id, dtype= float)
-
-    for user in user_id:
-        for business in business_id:
-            matrix.loc[business][user]= get_rating(REVIEWS, user, business)
-    return matrix
-
-def cosine_similarity(matrix=utility, business1, business2):
-    selected_features= matrix.loc[business1].notna() & matrix.loc[business2].notna()
-
-    if business1== business2:
-        return 1
-
-    if not selected_features.any():
-        return 0
-
-    features1= matrix.loc[business1][selected_features]
-    features2= matrix.loc[business2][selected_features]
-    
-    if (features1==0.0).all() or (features2==0.0).all():
-        return 0
-
-    num= (features1 * features2).sum()
-    den= np.sqrt((features1**2).sum()) * np.sqrt((features2**2).sum())
-
-    if num == 0 or den == 0:
-        return 0
-    else:
-        return num/den
-    
-def simMatrix(matrix=utility):
-    similiarityMatrix= pd.DataFrame(0, index=matrix.index, columns=matrix.index, dtype=float)
-
-    for business1 in BUSINESSES:
-        for business2 in BUSINESSES:
-            for user in USERS:
-                similiarityMatrix.xs(business1)[business2]= cosine_similarity(matrix, business1, business2)
-
-    return similiarityMatrix
-
-def meanCenteredSim(matrix= utility):
-    return matrix.sub(matrix.mean())
+def cosine_similarity_matrix(matrix=utilityMatrix):
+    meanCenteredMatrix= matrix - matrix.mean(axis=0)
+    return pd.DataFrame(pw.cosine_similarity(meanCenteredMatrix.fillna(0)), index= matrix.index, columns= matrix.index)
