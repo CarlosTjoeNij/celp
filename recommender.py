c9ce61f2aab3072efbd2b57cf2b70130b286ec98
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2019-05-16 16:46:42 +0200

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,16 +1,34 @@
 from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
-import sklearn.metrics.pairwise as pw
+
 import random
+import json
 import pandas as pd
 import numpy as np
 from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
-# create DataFrame with business name as index, categories as value
-businessdict= {}
-for city in CITIES:
-    for business in BUSINESSES[city]:
-        businessdict[business["name"]] = business["categories"]
-pd.DataFrame.from_dict(businessdict, orient= "index", columns=["categories"])
+def create_similarity_matrix_categories(matrix):
+    """Create a  similarity matrix"""
+    npu = matrix.values
+    m1 = npu @ npu.T
+    diag = np.diag(m1)
+    m2 = m1 / diag
+    m3 = np.minimum(m2, m2.T)
+    return pd.DataFrame(m3, index = matrix.index, columns = matrix.index)
+
+def extract_genres(businesses):
+    """Create an unfolded genre dataframe. Unpacks genres seprated by a '|' into seperate rows.
+    """
+    cat_b = businesses.apply(lambda row: pd.Series([row['business_id']] + row['categories'].lower().split(",")), axis=1)
+    stack_cat = cat_b.set_index(0).stack()
+    df_stack_cat = stack_cat.to_frame()
+    df_stack_cat['business_id'] = stack_cat.index.droplevel(1)
+    df_stack_cat.columns = ['categories', 'business_id']
+    return df_stack_cat.reset_index()[['business_id', 'categories']]
+
+def pivot_ratings(df):
+    return df.pivot_table(index = 'business_id', columns = 'categories', aggfunc = 'size', fill_value=0)
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
@@ -25,9 +43,20 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
             adress:str
         }
     """
-    if not city:
-        city = random.choice(CITIES)
-    return random.sample(BUSINESSES[city], n)
+
+    l=[]
+    businessdict= {}
+    for city in CITIES:
+        for business in BUSINESSES[city]:
+            businessdict[business["business_id"]] = business["categories"]
+    df_categories = pd.DataFrame.from_dict(businessdict, orient= "index", columns=['categories'])
+    df_categories['business_id'] = df_categories.index
+
+    extracted_categories = extract_categories(df_categories)
+    
+    gepivoteerde_data = pivot_ratings(extracted_categories)
+
+    sim = create_similarity_matrix_categories(gepivoteerde_data)
 
 # def eat_place(user_id, n):
     
@@ -36,3 +65,4 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
 #             categ = pd.Series(data=[business["categories"]])
 #             if categ.isin(["Food"]).any() or categ.isin(["Restaurants"]).any() or categ.isin(["Bars"]).any():
 #                 return BUSINESSES[business]
+
