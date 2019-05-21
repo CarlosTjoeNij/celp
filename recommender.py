79fcfb37a33c7907faa3d29d108bac0fe805155a
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2019-05-21 22:35:31 +0200

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -3,6 +3,7 @@ from helpers import *
 
 import random
 import data
+from data import *
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
@@ -17,31 +18,38 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
             adress:str
         }
     """
+    if user_id is not None:
+        # create dataframe containing business id and all categories
+        businessdict= {}
+        for city in CITIES:
+            for business in BUSINESSES[city]:
+                businessdict[business["business_id"]] = business["categories"]
+        df_categories = pd.DataFrame.from_dict(businessdict, orient= "index", columns=['categories'])
+        df_categories['business_id'] = df_categories.index
 
-    # create dataframe containing business id and all categories
-    businessdict= {}
-    for city in CITIES:
-        for business in BUSINESSES[city]:
-            businessdict[business["business_id"]] = business["categories"]
-    df_categories = pd.DataFrame.from_dict(businessdict, orient= "index", columns=['categories'])
-    df_categories['business_id'] = df_categories.index
+        # extract categories and compute similarity based of categories
+        extracted_categories = extract_categories(df_categories)
+        pivoted_data = pivot_ratings(extracted_categories)
+        sim = create_similarity_matrix_categories(pivoted_data)
 
-    # extract categories and compute similarity based of categories
-    extracted_categories = extract_categories(df_categories)
-    pivoted_data = pivot_ratings(extracted_categories)
-    sim = create_similarity_matrix_categories(pivoted_data)
+        # create dataframe containing reviews
+        for city in CITIES:
+            reviews = REVIEWS[city]
+            df_rev= pd.DataFrame.from_dict(json_normalize(reviews), orient='columns')
+        # delete duplicates, and keep most recent ones
+        df_review = df_rev.drop_duplicates(subset=["business_id", "user_id"], keep="last").reset_index()[["business_id", "stars", "user_id"]]
+        ratings_utility = create_utility_matrix(df_review)
 
-    # create dataframe containing reviews
-    for city in CITIES:
-        reviews = REVIEWS[city]
-        df_rev= pd.DataFrame.from_dict(json_normalize(reviews), orient='columns')
-    # delete duplicates, and keep most recent ones
-    df_review = df_rev.drop_duplicates(subset=["business_id", "user_id"], keep="last").reset_index()[["business_id", "stars", "user_id"]]
-    ratings_utility = create_utility_matrix(df_review)
+        # predict ratings
+        predicted = predict_ratings(sim, ratings_utility, df_review, 0)
+        sort_predicted = predicted.sort_values(by='predicted rating', ascending=False)
+        for_user = sort_predicted[sort_predicted['user_id'] == user_id][:10]
 
-    predicted = predict_ratings(sim, ratings_utility, df_review, 0)
-    return predicted.sort_values(by='predicted rating', ascending=False)[:10]
-
+        # return businesses
+        return [get_business(city, i) for i in for_user['business_id']]
+    else:
+        city = random.choice(CITIES)
+        return random.sample(BUSINESSES[city], n)
 # def eat_place(user_id, n):
     
 #     for city in CITIES:
